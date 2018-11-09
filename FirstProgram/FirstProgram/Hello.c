20cbfb52c2877a2995d0af7295c5c647938afc8b
Ecy <Ecy@ECYPC>
2018-11-09 12:25:34 +0800

    Add project files.

---
+++ FirstProgram/FirstProgram/Hello.c
@@ -0,0 +1,20 @@
+/*	Program Name: Hello.c
+	Written by	: Yeap Lee Zhi
+	Date		: 1/6/2016
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	printf("Hello there.\n");
+	printf("I love you.\n");
+	printf("Do you love me?\n");
+	system("pause");
+}
+
+/*Output:
+Hello there.
+I love you.
+Do you love me?*/
